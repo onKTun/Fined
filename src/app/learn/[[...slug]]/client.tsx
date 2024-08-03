62fb8e017ba114d80b360fd21ee2e74c9ea78dac
onKTun <kevinkltun@gmail.com>
2024-08-02 18:04:43 -0700

    created learn directory

--- src/app/learn/[[...slug]]/client.tsx
+++ src/app/learn/[[...slug]]/client.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import dynamic from "next/dynamic";
 
-const App = dynamic(() => import("../../../App"), { ssr: false });
+const App = dynamic(() => import("../../../App"), { ssr: false }); //dynamic client side rendering
 
 export function ClientOnly() {
   return <App />;
