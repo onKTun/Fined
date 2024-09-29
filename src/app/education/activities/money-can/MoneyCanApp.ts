7acbb0e71534c154bef6578b5f51f15cea1a5aee
onKTun <kevinkltun@gmail.com>
2024-09-29 15:16:26 -0700

    implemented draggability

--- src/app/education/activities/money-can/MoneyCanApp.ts
+++ src/app/education/activities/money-can/MoneyCanApp.ts
@@ -8,11 +8,14 @@ import {
   Container,
   Text,
   TextStyle,
+  DisplayObject,
 } from "pixi.js";
 import backgroundImage from "public/assets/backgrounds/fined_background_1.svg";
 
 const cardDimensions = { width: 187, height: 275, radius: 10 };
 
+let dragTarget: DisplayObject;
+let pixiApp: Application;
 const fontStyle = new TextStyle({
   fontFamily: "Helvetica",
   fontSize: 16,
@@ -21,14 +24,15 @@ const fontStyle = new TextStyle({
   align: "center",
 });
 
-export default function moneyCanScript(pixiApp: Application) {
-  setup(pixiApp);
-  propagateCards(testData, pixiApp);
+export default function moneyCanScript(app: Application) {
+  pixiApp = app;
+  setup();
+  propagateCards(testData);
   //main update loop
   pixiApp.ticker.add(() => {});
 }
 
-function setup(pixiApp: Application) {
+function setup() {
   //setup background
   const backgroundTexture = Texture.from(backgroundImage.src);
   const background = new Sprite(backgroundTexture);
@@ -65,9 +69,14 @@ function setup(pixiApp: Application) {
 
   // Add it to the stage to render
   pixiApp.stage.addChild(cardRegion);
+
+  pixiApp.stage.eventMode = "static";
+  pixiApp.stage.hitArea = pixiApp.screen;
+  pixiApp.stage.on("pointerup", onDragEnd);
+  pixiApp.stage.on("pointerupoutside", onDragEnd);
 }
 
-function propagateCards(jsonData: object, pixiApp: Application) {
+function propagateCards(jsonData: object) {
   const cardShape = new RoundedRectangle(
     0,
     0,
@@ -99,6 +108,89 @@ function propagateCards(jsonData: object, pixiApp: Application) {
     container.addChild(text);
 
     card.eventMode = "static";
+    card.on("pointerdown", onDragStart, card);
+
     console.log(card.isInteractive());
   }
 }
+
+function onDragMove(event) {
+  if (dragTarget) {
+    dragTarget.parent.toLocal(event.global, pixiApp.stage, dragTarget.position);
+  }
+}
+
+function onDragStart() {
+  // Store a reference to the data
+  // * The reason for this is because of multitouch *
+  // * We want to track the movement of this particular touch *
+  this.alpha = 0.5;
+  dragTarget = this;
+  pixiApp.stage.on("pointermove", onDragMove);
+}
+
+function onDragEnd() {
+  if (dragTarget) {
+    pixiApp.stage.off("pointermove", onDragMove);
+    dragTarget.alpha = 1;
+    //dragTarget = null;
+  }
+}
+
+/*
+
+    function createBunny(x, y)
+    {
+        // Create our little bunny friend..
+        const bunny = new Sprite(texture);
+
+        // Enable the bunny to be interactive... this will allow it to respond to mouse and touch events
+        bunny.eventMode = 'static';
+
+        // This button mode will mean the hand cursor appears when you roll over the bunny with your mouse
+        bunny.cursor = 'pointer';
+
+        // Center the bunny's anchor point
+        bunny.anchor.set(0.5);
+
+        // Setup events for mouse + touch using the pointer events
+        bunny.on('pointerdown', onDragStart, bunny);
+    }
+
+    let dragTarget = null;
+
+    app.stage.eventMode = 'static';
+    app.stage.hitArea = app.screen;
+    app.stage.on('pointerup', onDragEnd);
+    app.stage.on('pointerupoutside', onDragEnd);
+
+    function onDragMove(event)
+    {
+        if (dragTarget)
+        {
+            dragTarget.parent.toLocal(event.global, null, dragTarget.position);
+        }
+    }
+
+    function onDragStart()
+    {
+        // Store a reference to the data
+        // * The reason for this is because of multitouch *
+        // * We want to track the movement of this particular touch *
+        this.alpha = 0.5;
+        dragTarget = this;
+        app.stage.on('pointermove', onDragMove);
+    }
+
+    function onDragEnd()
+    {
+        if (dragTarget)
+        {
+            app.stage.off('pointermove', onDragMove);
+            dragTarget.alpha = 1;
+            dragTarget = null;
+        }
+    }
+})();
+
+*/
