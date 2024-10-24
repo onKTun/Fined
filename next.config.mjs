63d3431a11da9790705c0c3ab67c2a1192046cfb
onKTun <kevinkltun@gmail.com>
2024-10-23 20:29:25 -0700

    implemented Strict-Transport-Security X-Content-Type-Option X-Frame-Options

--- next.config.mjs
+++ next.config.mjs
@@ -26,6 +26,18 @@ const nextConfig = {
             key: "Content-Security-Policy",
             value: cspHeader.replace(/\n/g, ""),
           },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
         ],
       },
     ];
