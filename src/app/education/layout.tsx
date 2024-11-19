0cb74972ac984937f3abb77d9ef8ed11f238b183
“Adam <adamsandy38@gmail.com>
2024-11-18 19:08:44 -0800

    quick bug fix

--- src/app/education/layout.tsx
+++ src/app/education/layout.tsx
@@ -3,11 +3,7 @@
 import React from "react";
 
 import { getUserAndCache } from "utils/supabase/user";
-import ClientsideEducationLayout from "src/components/layouts/ClientsideEducationLayout";
-import DefaultHeader from "src/components/header/DefaultHeader";
-import Header from "src/components/header/Header";
-import Sidebar from "src/components/sidebar/Sidebar";
-import { SidebarProvider } from "src/components/sidebar/sidebarContext";
+import ClientsideEducationLayout from "src/components/layouts/clientsideEducationLayout";
 
 export default async function RootLayout({ children }) {
   const user = await getUserAndCache();
