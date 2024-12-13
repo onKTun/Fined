33ce9e96c3bccc436e7df5197899b6b65acef1d3
onKTun <kevinkltun@gmail.com>
2024-12-13 02:26:32 -0800

    updated slug naming

--- src/app/education/units/[slug]/page.tsx
+++ src/app/education/units/[slug]/page.tsx
@@ -1,5 +1,10 @@
 import ClientsideUnit from "src/components/units/ClientsideUnit";
 
+// Return a list of `params` to populate the [slug] dynamic segment
+export async function generateStaticParams() {
+  return [{ slug: "1" }];
+}
+
 export default function UnitPage({ params }: { params: { slug: string } }) {
   return <ClientsideUnit></ClientsideUnit>;
 }
