7efba8ed694992d31357d5bcabb4b1494e486a8f
onKTun <kevinkltun@gmail.com>
2024-12-13 17:13:48 -0800

    hotfix for video dynamic route

--- src/app/education/units/[unitSlug]/[lessonPreviewSlug]/[videoLessonSlug]/page.tsx
+++ src/app/education/units/[unitSlug]/[lessonPreviewSlug]/[videoLessonSlug]/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import styles from "./lessonvideo.module.css";
 import Video from "./components/videocomponent/Video";
 import { VideoProvider } from "./hooks/VideoContext";
